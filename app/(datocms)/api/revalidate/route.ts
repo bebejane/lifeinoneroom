5ddae4ad97997dccd46c0f4aaf9729803fe77b6b
Bebe Jane <bjornthief@gmail.com>
2024-05-21 14:06:37 +0200

    reval

--- app/(datocms)/api/revalidate/route.ts
+++ app/(datocms)/api/revalidate/route.ts
@@ -7,13 +7,21 @@ export async function POST(req: Request) {
 
   return await revalidate(req, async (payload, revalidate) => {
 
-    const { api_key, entity, event_type, entity_type } = payload;
-    const { id, attributes: { slug } } = entity
+    const { api_key, entity } = payload;
+    const { id } = entity
     const paths: string[] = []
     const tags: string[] = [api_key, id].filter(t => t)
 
     switch (api_key) {
-
+      case 'post': case 'image':
+        paths.push(`/`)
+        break
+      case 'about':
+        paths.push(`/`)
+        paths.push(`/about`)
+        break
+      default:
+        break
     }
 
     return await revalidate(paths, tags, true)
