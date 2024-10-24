7498758f5607a713167bcd8f5732c17200b3eab2
Bebe Jane <bjornthief@gmail.com>
2024-10-24 20:37:49 +0200

    scroll line

--- app/components/posts/TextPost.tsx
+++ app/components/posts/TextPost.tsx
@@ -3,11 +3,12 @@
 import s from './TextPost.module.scss'
 import cn from 'classnames'
 import Content from '@components/Content';
-import { useContext, useEffect, useRef, useState } from 'react';
+import { use, useCallback, useContext, useEffect, useRef, useState } from 'react';
 import { useStore } from '@lib/store';
 import PublishDate from '../PublishDate';
 import AudioPlayer from '../AudioPlayer';
 import { Theme, ThemeContext } from '@components/theme/ThemeContext';
+import { useScrollInfo } from 'next-dato-utils/hooks';
 
 export type LayoutProps = {
   data: TextRecord
@@ -20,26 +21,22 @@ export default function TextPost({ data: { id, title, text, audio, textColor, ba
   const [open, setOpen] = useState(true)
   const [lineStyles, setLineStyles] = useState<{ top: React.CSSProperties, bottom: React.CSSProperties, line: React.CSSProperties } | null>(null)
   const ref = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    setOpen(expanded)
-    !expanded && setLineStyles(null)
-  }, [expanded])
+  const clientY = useRef<number>(0)
 
   const handleClick = () => {
     !expanded && setOpen(!open)
-    const shouldScroll = !expanded && !open
-    shouldScroll && setTimeout(() => {
+    !expanded && !open && setTimeout(() => {
       document.getElementById(id).scrollIntoView({ behavior: 'smooth', block: 'start' })
     }, 100)
   }
 
-  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+  const handleMouseMove = useCallback((e?: React.MouseEvent<HTMLDivElement>) => {
+
     if (!settings.readingline) return
 
-    const { clientY } = e
+    clientY.current = e?.clientY ?? clientY.current
     const { y, height } = ref.current.getBoundingClientRect()
-    const yPercent = (clientY - y) / height * 100
+    const yPercent = (clientY.current - y) / height * 100
     const lineHeight = '8rem'
 
     setLineStyles({
@@ -47,8 +44,20 @@ export default function TextPost({ data: { id, title, text, audio, textColor, ba
       bottom: { flexBasis: `calc(${100 - yPercent}% - ${lineHeight})` },
       line: { flexBasis: `${lineHeight}` }
     })
-  }
+  }, [settings.readingline])
+
   const sectionStyle = (theme !== 'dark' && settings.colors) ? { backgroundColor: backgroundColor?.hex, color: textColor?.hex } : undefined
+
+  useEffect(() => {
+    setOpen(expanded)
+    !expanded && setLineStyles(null)
+  }, [expanded])
+
+  useEffect(() => {
+    const handleScroll = () => handleMouseMove()
+    document.addEventListener('scroll', handleScroll)
+    return () => document.removeEventListener('scroll', handleScroll)
+  }, [handleMouseMove])
 
   return (
     <section
@@ -58,13 +67,14 @@ export default function TextPost({ data: { id, title, text, audio, textColor, ba
       className={cn(s.text, open && s.open)}
       onClick={handleClick}
       onMouseMove={handleMouseMove}
+      onScroll={handleMouseMove}
       onMouseLeave={() => setLineStyles(null)}
       style={sectionStyle}
     >
       {open ?
         <>
           <h3 className={s.title}>{title}</h3>
-          <AudioPlayer audio={audio} open={open} show={lineStyles && open} fullMargin={true} />
+          <AudioPlayer audio={audio} open={open} show={lineStyles && open} fullMargin={false} />
           <Content content={text} />
           {lineStyles &&
             <div className={cn(s.readingline, lineStyles && s.show)}>
