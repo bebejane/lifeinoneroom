b94d32b13db6a7de6e9d2ca843c70654213c7395
Bebe Jane <bjornthief@gmail.com>
2025-02-20 17:29:30 +0100

    fugg

--- app/feed/route.ts
+++ app/feed/route.ts
@@ -2,8 +2,6 @@ import { getAllPosts } from "../../lib/posts";
 import { render } from 'datocms-structured-text-to-plain-text';
 import { Feed } from "feed";
 
-export const revalidate = 60 * 60 * 6;
-
 export async function GET(request: Request) {
   const { allPosts } = await getAllPosts()
 
