7b49eb8622b403082552f993be3334fb60929175
Bebe Jane <bjornthief@gmail.com>
2025-02-20 18:37:33 +0100

    links

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,28 +1,26 @@
-import { StructuredContent } from 'next-dato-utils/components'
+import StructuredContent from './StructuredContent';
 //import * as Blocks from './blocks/index'
-const Blocks = {}
+const Blocks = {};
 
 export type Props = {
-  id?: string
-  content: any
-  styles?: any
-  className?: string
-  blocks?: any
-}
+	id?: string;
+	content: any;
+	styles?: any;
+	className?: string;
+	blocks?: any;
+};
 
 export default function Content({ id, content, styles, blocks, className }: Props) {
+	if (!content) return null;
 
-  if (!content)
-    return null
-
-  return (
-    <StructuredContent
-      blocks={{ ...Blocks, ...blocks }}
-      className={className}
-      styles={{
-        ...styles,
-      }}
-      content={content}
-    />
-  )
+	return (
+		<StructuredContent
+			blocks={{ ...Blocks, ...blocks }}
+			className={className}
+			styles={{
+				...styles,
+			}}
+			content={content}
+		/>
+	);
 }
---
+++ components/StructuredContent.tsx
@@ -0,0 +1,162 @@
+import { StructuredText, renderNodeRule, renderMarkRule } from 'react-datocms';
+import { isParagraph, isHeading, isRoot, isLink } from 'datocms-structured-text-utils';
+
+export type Props = {
+	content: any;
+	className?: string;
+	blocks?: any;
+	styles?: { [key: string]: string };
+};
+
+export default function StructuredContent({
+	content,
+	className,
+	blocks,
+	styles,
+}: //onClick
+Props) {
+	if (!content) return null;
+
+	const customMarkRules =
+		(styles &&
+			Object.keys(styles).map((style) =>
+				renderMarkRule(style, ({ mark, children, key }) => {
+					return (
+						<span key={key} className={styles[style]}>
+							{children}
+						</span>
+					);
+				})
+			)) ||
+		[];
+
+	return (
+		<StructuredText
+			data={content}
+			renderBlock={({ record }) => {
+				const Block = blocks[record?.__typename?.replace('Record', '')];
+				if (!Block) return null;
+				return <Block key={record?.id} data={record} />;
+				////onClick={(id: string) => onClick?.(id)}
+			}}
+			renderInlineRecord={({ record }) => {
+				switch (record.__typename) {
+					default:
+						return null;
+				}
+			}}
+			renderLinkToRecord={({ record, children, transformedMeta }) => {
+				switch (record.__typename) {
+					default:
+						return null;
+				}
+			}}
+			renderText={(text) => {
+				// Replace nbsp, quotes and multiple spaces
+				return text?.replace(/\s/g, ' ')?.replaceAll('"', '”');
+			}}
+			customMarkRules={customMarkRules}
+			customNodeRules={[
+				// Clenup paragraphs
+				renderNodeRule(
+					isParagraph,
+					({ adapter: { renderNode }, node, children, key, ancestors }) => {
+						const firstChild = node.children[0];
+						const lastChild = node.children[node.children.length - 1];
+
+						// Remove trailing <br>
+						if (
+							isRoot(ancestors[0]) &&
+							lastChild.type === 'span' &&
+							lastChild.value?.endsWith('\n')
+						) {
+							let index = node.children.length;
+
+							while (index >= 0 && firstChild.type === 'span' && firstChild.value[index] === '\n')
+								index--;
+
+							// remove trailing br
+							if (children && Array.isArray(children) && typeof children[0] === 'object')
+								Array.isArray(children[0].props.children) &&
+									children[0].props.children.splice(index);
+						}
+
+						////@ts-ignore // Remove leading <br>
+						if (
+							isRoot(ancestors[0]) &&
+							firstChild.type === 'span' &&
+							firstChild.value.startsWith('\n')
+						) {
+							let index = 0;
+
+							while (index < firstChild.value.length && firstChild.value[index] === '\n') index++;
+
+							if (children && Array.isArray(children) && typeof children[0] === 'object')
+								Array.isArray(children[0].props.children) &&
+									children[0].props.children?.splice(0, index + 1);
+						}
+
+						// Filter out empty paragraphs
+						children = children?.filter(
+							(c) =>
+								!(typeof c === 'object' && c.props.children?.length === 1 && !c.props.children[0])
+						);
+
+						// If no children remove tag completely
+						if (!children?.length) return null;
+
+						const classNames = [];
+
+						isRoot(ancestors[0]) && className && classNames.push(className);
+						node.style && styles?.[node.style] && classNames.push(styles[node.style]);
+
+						node.style &&
+							!styles?.[node.style] &&
+							console.warn(node.style, 'does not exist in styles', 'P');
+
+						// Return paragraph with sanitized children
+						return renderNode(
+							'p',
+							{
+								key,
+								className: classNames.length ? classNames.join(' ') : undefined,
+							},
+							children
+						);
+					}
+				),
+				// Add H classes
+				renderNodeRule(isHeading, ({ adapter: { renderNode }, node, children, key, ancestors }) => {
+					const classNames: string[] = [];
+					node.style && styles?.[node.style] && classNames.push(styles[node.style]);
+					node.style &&
+						!styles?.[node.style] &&
+						console.warn(node.style, 'does not exist in styles', 'H');
+
+					return renderNode(
+						`h${node.level}`,
+						{
+							key,
+							className: classNames.length ? classNames.join(' ') : undefined,
+						},
+						children
+					);
+				}),
+				renderNodeRule(isLink, ({ adapter: { renderNode }, node, children, key, ancestors }) => {
+					//@ts-ignore
+					const text = children?.[0]?.props?.children?.[0] ?? '';
+					return renderNode(
+						`a`,
+						{
+							key,
+							href: node.url,
+							target: '_blank',
+							'aria-label': `${text} (opens in a new tab)`,
+						},
+						children
+					);
+				}),
+			]}
+		/>
+	);
+}
